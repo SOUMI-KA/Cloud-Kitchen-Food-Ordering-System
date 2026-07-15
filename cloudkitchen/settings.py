550874ed70f58b24fee9fd435f4bff6e464df769
SOUMI-KA <soumika171@gmail.com>
2026-07-15 11:23:25 +0530

    Fix static files

--- cloudkitchen/settings.py
+++ cloudkitchen/settings.py
@@ -138,4 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
